3eeaeaa84935a402f8cead45298089e8afaf51c9
Paige <ecalvert@pivotal.io>
2020-03-11 16:16:06 -0600

    Update comment above updated redirect [#171503369]

--- redirects.rb
+++ redirects.rb
@@ -3,6 +3,6 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
   rack_env['SERVER_NAME'] == 'docs.pivotal.io' && rack_env['HTTP_X_FORWARDED_PROTO'] == 'http'
 }
 
-# Redirects for docs-book-healthwatch
+# Redirects for docs-book-metric-store
 
 r301 %r{/metric-store/(?![\d-]+)(.*)}, "/metric-store/0-3/$1"
