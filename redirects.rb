e043b71e3221c2460f2219c3261f336e7f20a0e2
Samia Nneji <snneji@vmware.com>
2022-05-17 16:49:39 +0100

    Fix redirects for metric store [#182195205]

--- redirects.rb
+++ redirects.rb
@@ -8,22 +8,10 @@ r301 %r{.*}, 'https://docs.pivotal.io$&', :if => Proc.new { |rack_env|
 r301 %r{/metric-store/(?![\d-]+)(.*)}, "/metric-store/1-5/$1"
 
 # Redirect from docs.pivotal.io/metric-store/1-3 to docs.vmware.com Metric Store docs
-
-r301  %r{/metric-store/1-3/index.html}, "https://docs.vmware.com/en/Metric-Store/1.3/metric-store/GUID-index.html"
-r301  %r{/metric-store/1-3/}, "https://docs.vmware.com/en/Metric-Store/1.3/metric-store/GUID-index.html"
-r301  %r{/metric-store/1-3}, "https://docs.vmware.com/en/Metric-Store/1.3/metric-store/GUID-index.html"
 r301  %r{/metric-store/1-3/(.*)}, "https://docs.vmware.com/en/Metric-Store/1.3/metric-store/GUID-$1"
 
 # Redirect from docs.pivotal.io/metric-store/1-4 to docs.vmware.com Metric Store docs
-
-r301  %r{/metric-store/1-4/index.html}, "https://docs.vmware.com/en/Metric-Store/1.4/metric-store/GUID-index.html"
-r301  %r{/metric-store/1-4/}, "https://docs.vmware.com/en/Metric-Store/1.4/metric-store/GUID-index.html"
-r301  %r{/metric-store/1-4}, "https://docs.vmware.com/en/Metric-Store/1.4/metric-store/GUID-index.html"
 r301  %r{/metric-store/1-4/(.*)}, "https://docs.vmware.com/en/Metric-Store/1.4/metric-store/GUID-$1"
 
 # Redirect from docs.pivotal.io/metric-store/1-5 to docs.vmware.com Metric Store docs
-
-r301  %r{/metric-store/1-5/index.html}, "https://docs.vmware.com/en/Metric-Store/1.5/metric-store/GUID-index.html"
-r301  %r{/metric-store/1-5/}, "https://docs.vmware.com/en/Metric-Store/1.5/metric-store/GUID-index.html"
-r301  %r{/metric-store/1-5}, "https://docs.vmware.com/en/Metric-Store/1.5/metric-store/GUID-index.html"
 r301  %r{/metric-store/1-5/(.*)}, "https://docs.vmware.com/en/Metric-Store/1.5/metric-store/GUID-$1"
